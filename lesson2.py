da4a07dfdd679c2f6f9ebfa0c4e148487cfe28c1
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-07-06 13:31:14 +0500

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+homework_complete = 12
+hours = 1.5
+course_name = "Pyton"
+average_time = homework_complete/hours
+print('Курс: ' + course_name + ', всего задач:', str(homework_complete) + ', затрачено часов:', str(hours) + ', среднее время выполнения:', str(average_time), 'часов')
