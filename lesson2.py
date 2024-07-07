35996b3a9d3969ad886fad56606813ecd53272c0
bucks22 <121617528+bucks22@users.noreply.github.com>
2024-07-07 15:26:57 +0500

    Урок Переменные_исправлени
    Урок Переменные_исправлени

--- lesson2.py
+++ lesson2.py
@@ -1,5 +1,5 @@
 homework_complete = 12
 hours = 1.5
 course_name = "Pyton"
-average_time = homework_complete/hours
+average_time = hours/homework_complete
 print('Курс: ' + course_name + ', всего задач:', str(homework_complete) + ', затрачено часов:', str(hours) + ', среднее время выполнения:', str(average_time), 'часов')
